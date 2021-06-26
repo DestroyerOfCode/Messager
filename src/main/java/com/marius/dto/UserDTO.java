4e06642c23e4a69842fd4cbca309f492ff98a822
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-06-26 13:43:41 +0200

    added model for user

---
+++ src/main/java/com/marius/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.marius.dto;
+
+import java.util.UUID;
+
+public class UserDTO {
+
+    private UUID id;
+    private String cityName;
+    private String phoneNumber;
+}
---
+++ src/main/java/com/marius/model/User.java
@@ -0,0 +1,29 @@
+package com.marius.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Document(value = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @NotNull(message = "The value id must be NOT null and hence must be set!")
+    private ObjectId id;
+
+    @NotBlank
+    private String cityName;
+    @NotBlank
+    private String phoneNumber;
+}
