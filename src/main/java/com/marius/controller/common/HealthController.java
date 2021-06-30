ad77bb087f772920a903d62a743a4b85e7643bfd
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-06-30 13:55:12 +0200

    added health endpoint for post

--- src/main/java/com/marius/controller/common/HealthController.java
+++ src/main/java/com/marius/controller/common/HealthController.java
@@ -1,8 +1,10 @@
 package com.marius.controller.common;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/health")
 public class HealthController {
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getHealth() {
+        return new ResponseEntity<>("Som OK! Bezim!", HttpStatus.OK);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postHealth() {
         return new ResponseEntity<>("Som OK! Bezim!", HttpStatus.OK);
     }
 }
