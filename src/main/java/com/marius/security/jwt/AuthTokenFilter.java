cd07c8aa47b7550ebba058f0acfa4fe335efffe9
DestroyerOfCode <marius.babkovic9@gmail.com>
2022-01-27 13:52:52 +0100

    refactor in jwt auth

--- src/main/java/com/marius/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/marius/security/jwt/AuthTokenFilter.java
@@ -39,17 +39,21 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
-                        userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                authenticateUser(request, userDetails);
             }
         } catch (Exception e) {
             logger.error("cannot set user authentication", e);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void authenticateUser(HttpServletRequest request, UserDetails userDetails) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     private String parseJwt(HttpServletRequest request) {
